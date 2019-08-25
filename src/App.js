620f6bab16873400ae45f94a98cb2b357c3ec13e
robertistok <robertistok94@gmail.com>
2019-08-25 19:59:20 +0200

    Add description

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Root>
-        Wlecome!
+        <Description>
+          Welcome to memory-flip. Start by clicking on any of the cards and try
+          to find its matching pair.
+        </Description>
         <CardBoard />
       </Root>
     </ThemeProvider>
@@ -19,5 +22,13 @@ const Root = styled.div`
   height: calc(100vh - 40px);
   width: calc(100vw - 40px);
   padding: 20px;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Description = styled.p`
+  margin: 40px;
 `;
 export default App;
